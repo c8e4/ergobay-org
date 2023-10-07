1619edbfae3f6d5c5a38c811cc7e4a1ebe350938
c8e4 <c8e4@proton.me>
2023-10-07 17:25:44 +0200

    update contract, add buy/sell tx

--- src/lib/contract/SellForErg.ts
+++ src/lib/contract/SellForErg.ts
@@ -2,15 +2,14 @@ export const SellForErg = `
 {
     val priceNanoErg     	= SELF.R4[Long].get         // token sale price in nanoErg
     val sellerPK 			= SELF.R5[GroupElement].get // Public Key of the token seller
-    val sellerAddress		= SELF.R6[Coll[Byte]].get   // seller receive address
-    val feeAddress			= SELF.R7[Coll[Byte]].get   // ui Fee receive address
+    val devPK			    = SELF.R6[GroupElement].get // dev Public Key
 
     val feeDenom 			= 100000L
-    val devFee   			= 1000L
-    val feeInNanoErg   		= (priceNanoErg.toBigInt * devFee.toBigInt) / feeDenom.toBigInt
+    val feeNum   			= 1000L                     // 1% fee
+    val feeInNanoErg   		= (priceNanoErg.toBigInt * feeNum.toBigInt) / feeDenom.toBigInt
     val onlyOneBoxSpent		= (OUTPUTS(0).R4[Coll[Byte]].get == SELF.id)
-    val sellerHappy			= (OUTPUTS(0).value >= priceNanoErg - feeInNanoErg) && (OUTPUTS(0).propositionBytes == sellerAddress)
+    val sellerHappy			= (OUTPUTS(0).value >= priceNanoErg - feeInNanoErg) && (OUTPUTS(0).propositionBytes == sellerPK.propBytes)
     val feePaid				= (feeInNanoErg == 0) ||
-                              (OUTPUTS(1).value >= feeInNanoErg && OUTPUTS(1).propositionBytes == feeAddress)
+                              (OUTPUTS(1).value >= feeInNanoErg && OUTPUTS(1).propositionBytes == devPK.propBytes)
     sigmaProp(onlyOneBoxSpent && sellerHappy && feePaid) || proveDlog(sellerPK)
 }`
